2227003ee76c689a36d80359f1d2eadc1ec63149
Ivan <mmavrin80@gmail.com>
2022-04-03 19:19:18 +0300

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿int Parse(string message)
+{
+    Console.WriteLine(message);
+    string strNum = Console.ReadLine();
+    int Num = int.Parse(strNum);
+    return Num;
+}
+
+int Result(int arg)
+{
+    int res = 0;
+    int temp;
+    for (int i = 1; i <= arg; i++)
+    {
+        temp = Parse($"Введите число {i}");
+        if (temp > 0)
+        {
+            res++;
+        }
+    }
+    return res;
+}
+
+int Amount = Parse("Сколько чисел вы хотите ввести: ");
+Console.WriteLine($"Чисел больше нуля - {Result(Amount)}");
